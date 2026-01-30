82ebddf6414c22d135670204131927cb4646ab8d
RENgOLD <rengold@me.com>
2026-01-31 03:38:56 +0400

    strings

---
+++ 2_strings/full_name.py
@@ -0,0 +1,24 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print("")
+
+print("Python")
+print("\tPython")
+print("")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+print("")
+
+favorite_language = " python "
+print(favorite_language)
+print(favorite_language.lstrip())
+print(favorite_language.rstrip())
+print(favorite_language.strip())
+print("")
+
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url)
+sumple_url = nostarch_url.removeprefix('https://')
+print(sumple_url)
