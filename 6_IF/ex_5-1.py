ae594f0eff6e3773574e8fc5dd510d02d8791bf0
RENgOLD <rengold@me.com>
2026-02-06 17:33:32 +0400

    Loop exercise 5.1

---
+++ 6_IF/ex_5-1.py
@@ -0,0 +1,19 @@
+# 5.1. Проверка условий. Напишите последовательность условий. Выведите описание
+# каждой проверки и ваш прогноз относительно ее результата. Код должен выглядеть
+# примерно так:
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+# • Внимательно просмотрите результаты. Убедитесь в том, что понимаете, почему
+# результат каждой строки равен True или False.
+# • Создайте как минимум 10 условий. Не менее пяти одних должны давать результат
+# True, а не менее пяти других — результат False.
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
