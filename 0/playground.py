9a616a08a6a65bdb7d08bc940e07948e07737cfd
RENgOLD <rengold@me.com>
2026-03-17 07:49:24 +0400

    Files exercise 10.1

--- 0/playground.py
+++ 0/playground.py
@@ -1,17 +1,10 @@
-def get_formatted_name(first_name: object, last_name: object, middle_name: object = None) -> str:
-    """Возвращает отформатированное полное имя."""
-    if '' == False:
-        print("'' == False")
-    if None == False:
-        print('None == False')
-        
-    if middle_name:
-        full_name = f"{first_name} {middle_name} {last_name}"
-    else:
-        full_name = f"{first_name} {last_name}"
-    return full_name.title()
+from pathlib import Path
 
-musician = get_formatted_name('jimi', 'hendrix')
-print(musician)
-musician = get_formatted_name('john', 'hooker', 'lee')
-print(musician)
+path = Path("../10_Files/pi_digits.txt")
+print(path)
+contents = path.read_text()
+lines = contents.splitlines()
+pi = ''
+for line in lines:
+    pi = f'{pi}{line}'
+print(pi)
