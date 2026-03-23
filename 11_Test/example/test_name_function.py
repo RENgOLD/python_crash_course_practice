f85a5edb832f9a245cfc6a185975cddd84ce38bf
RENgOLD <rengold@me.com>
2026-03-23 17:47:27 +0400

    Tests example update

--- 11_Test/example/test_name_function.py
+++ 11_Test/example/test_name_function.py
@@ -1,5 +1,7 @@
 from name_function import get_formatted_name
 
 def test_first_last_name():
-    formatted_name = get_formatted_name('first', 'last')
-    assert formatted_name == 'First Last'
+    assert get_formatted_name('first', 'last') == 'First Last'
+
+def test_first_last_middle_name():
+    assert get_formatted_name('first','last','middle') == 'First Middle Last'
